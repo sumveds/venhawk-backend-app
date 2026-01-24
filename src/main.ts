db621f84dba521012d700d09764f69e90cfe1f59
Sumved Shami <sumved.shami@gmail.com>
2026-01-25 02:53:06 +0530

    fixing cors issue.

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS
+  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [
+    'http://localhost:5173',
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: allowedOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
